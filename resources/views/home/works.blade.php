636b9a57ad944c081bf0cc2b760a57726959ad05
Hamza <hamzaamir72007@gmail.com>
2025-12-08 02:08:17 +0500

    redesign one

---
+++ resources/views/home/works.blade.php
@@ -0,0 +1,79 @@
+<div
+        class="section w-full max-w-[1920px] flex justify-between max-[910px]:justify-center gap-2 items-center flex-wrap p-[50px] max-[720px]:p-5 max-[720px]:flex-col">
+        <div class="card flex flex-col gap-2 w-full max-w-[400px] p-2 rounded-md">
+            <h1 class="text-5xl font-bold text-lime-700">
+                How It Works
+            </h1>
+            <p class="text-gray-950 font-semibold">
+                We connect buyers and vendors by providing them with the most trusted, reliable, and insightful content
+                available, helping both sides make confident and informed decisions.
+            </p>
+            <button class="bg-lime-700 p-2 text-center font-bold text-white rounded-md">Watch Top Companies</button>
+            <button class="outline-lime-700 outline-2 p-2 text-center font-bold text-lime-700 rounded-md">Get Help</button>
+        </div>
+        <div class="card relative flex flex-col gap-2 w-full max-w-[400px] hover:bg-lime-700/50 p-2 rounded-md">
+            <img class="w-[400px] aspect-video object-cover rounded"
+                src="https://img.freepik.com/free-photo/colleagues-working-project-discussing-details_114579-2817.jpg?t=st=1763479584~exp=1763483184~hmac=7af7a3602e294d4a89844615ea69929bfca37051b00c55f0a03b88aa2e794cb1&w=1060"
+                alt="">
+            <h1 class="text-xl font-bold text-lime-700">
+                Define important parameters
+            </h1>
+            <p class="text-gray-950 font-semibold">
+                Start by identifying the key parameters and requirements for your project
+            </p>
+            <span
+                class="bg-lime-700 aspect-square absolute w-[50px] flex items-center justify-center font-bold text-white rounded-md m-2 top-0 left-0 p-2">1</span>
+        </div>
+        <div class="card relative flex flex-col gap-2 w-full max-w-[400px] hover:bg-lime-700/50 p-2 rounded-md">
+            <img class="w-[400px] aspect-video object-cover rounded"
+                src="https://img.freepik.com/free-photo/female-hand-typing-keyboard-laptop_1150-15742.jpg?t=st=1763480073~exp=1763483673~hmac=685bef002edd1687dd7b588d41c71d42eefee740df9863ed2b2bbd59aa93db89&w=740"
+                alt="">
+            <h1 class="text-xl font-bold text-lime-700">
+                Study the options
+            </h1>
+            <p class="text-black font-semibold">
+                Review top companies their case studies, services, and capabilities
+            </p>
+            <span
+                class="bg-lime-700 aspect-square absolute w-[50px] flex items-center justify-center font-bold text-white rounded-md m-2 top-0 left-0 p-2">2</span>
+        </div>
+        <div class="card relative flex flex-col gap-2 w-full max-w-[400px] hover:bg-lime-700/50 p-2 rounded-md">
+            <img class="w-[400px] aspect-video object-cover rounded"
+                src="https://img.freepik.com/free-photo/front-view-business-man-holding-tablet_23-2150103598.jpg?t=st=1763480258~exp=1763483858~hmac=22477c833b9e10cb60d895f4a7227ad6c983d5e53dd6db0992037a41b6109d25&w=740"
+                alt="">
+            <h1 class="text-xl font-bold text-lime-700">
+                Study a company’s details
+            </h1>
+            <p class="text-black font-semibold">
+                Review each company's profile to ensure they meet your expectations and needs
+            </p>
+            <span
+                class="bg-lime-700 aspect-square absolute w-[50px] flex items-center justify-center font-bold text-white rounded-md m-2 top-0 left-0 p-2">3</span>
+        </div>
+        <div class="card relative flex flex-col gap-2 w-full max-w-[400px] hover:bg-lime-700/50 p-2 rounded-md">
+            <img class="w-[400px] aspect-video object-cover rounded"
+                src="https://img.freepik.com/free-photo/business-women-signature-document_1388-90.jpg?t=st=1763480323~exp=1763483923~hmac=915ac3828db4b8560409aa372425116d7c8e1e9f43794d84e567b0d77ce15a2c&w=740"
+                alt="">
+            <h1 class="text-xl font-bold text-lime-700">
+                Shortlist companies
+            </h1>
+            <p class="text-black font-semibold">
+                Shortlist companies that fit your needs and project easily
+            </p>
+            <span
+                class="bg-lime-700 aspect-square absolute w-[50px] flex items-center justify-center font-bold text-white rounded-md m-2 top-0 left-0 p-2">4</span>
+        </div>
+        <div class="card relative flex flex-col gap-2 w-full max-w-[400px] hover:bg-lime-700/50 p-2 rounded-md">
+            <img class="w-[400px] aspect-video object-cover rounded"
+                src="https://img.freepik.com/free-photo/businessman-handshaking-businesswoman-showing-respect-closeup-view-hands-shaking_1163-4679.jpg?t=st=1763480429~exp=1763484029~hmac=ac218bd01b5112f29e3aaf87f469044eb877d28328455877b031290e8dbada71&w=740"
+                alt="">
+            <h1 class="text-xl font-bold text-lime-700">
+                Partner with the Best
+            </h1>
+            <p class="text-black font-semibold">
+                Select the top company from your shortlist and start building your partnership
+            </p>
+            <span
+                class="bg-lime-700 aspect-square absolute w-[50px] flex items-center justify-center font-bold text-white rounded-md m-2 top-0 left-0 p-2">5</span>
+        </div>
+    </div>
