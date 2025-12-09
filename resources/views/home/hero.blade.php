236310212ca51397ad5b173f3ada358f779e67be
Hamza <hamza47@gmail.com>
2025-12-09 16:48:57 +0500

    Hero Section Completed

--- resources/views/home/hero.blade.php
+++ resources/views/home/hero.blade.php
@@ -1,123 +1,106 @@
-<div class="section mt-21 flex items-center text-white flex-wrap">
-    <div class="flex flex-col gap-4">
+<div class="section md:px-10 sm:px-6 px-3 md:py-12  md:pt-32 pt-25  flex items-center text-white flex-wrap gap-y-6 lg:flex-row flex-col">
+    <div class="flex flex-col items-center lg:items-start gap-4 lg:text-start text-center flex-1 ">
         <h1 class="text-4xl sm:text-4xl md:text-5xl lg:text-6xl max-w-[900px] font-bold ">
             Where <span class="text-lime-700">Businesses</span>
             Meet Their Ideal Development
             <span class="text-lime-700">Partners</span>
         </h1>
-        <p class="w-full leading-6 max-w-[900px] font-semibold">At topratedcompanies, we cut through market noise
+        <p class="w-full md:leading-6 leading-5 max-w-[900px] font-semibold text-gray-300 md:text-base text-sm">At topratedcompanies, we cut through market
+            noise
             and simplify
             your
             search with curating
             credible, experienced, and dependable companies.
             Explore comprehensive profiles and choose your next partner with clarity and confidence.
         </p>
-        <p class=" w-full max-w-[900px] font-semibold">Explore comprehensive profiles and choose your next partner
+        <p class=" w-full max-w-[900px] font-semibold text-gray-300 md:text-base text-sm">Explore comprehensive profiles and choose your next
+            partner
             with
             clarity and confidence.
         </p>
-        <div class="flex items-center  w-max bg-white rounded-md h-11 py-0.5 px-0.5">
-            <div class="flex flex-col max-w-72 flex-1 h-full relative">
+        {{-- !Search Box --}}
+        <div class="flex items-center md:w-4/5 lg:w-4/5 w-full bg-white rounded-md md:h-11 h-9 py-0.5 px-0.5">
+            <div class="flex flex-col flex-1 h-full relative">
                 <div
-                    class="flex items-center px-3 gap-2 bg-white border-gray-500/30 h-full rounded-md overflow-hidden">
+                    class="flex flex-1 items-center md:px-3 px-1 gap-2 bg-white border-gray-500/30 h-full rounded-md overflow-hidden">
                     <input type="text" placeholder="Service you need"
-                        class="peer w-full h-full outline-none text-gray-500 placeholder-gray-500 text-sm">
-                        <i class="fa-solid fa-chevron-down  text-gray-400 "></i>
+                        class="peer w-full h-full outline-none text-gray-700 placeholder-gray-500 md:text-sm text-xs">
+                    <i class="fa-solid fa-chevron-down  text-gray-400 text-[10px] sm:text-base"></i>
 
                     <div
-                        class="absolute top-full left-0 right-0 mt-1.5 py-2 bg-white border border-gray-500/30 rounded-md overflow-hidden opacity-0 invisible transition-all duration-200 peer-focus:opacity-100 peer-focus:visible z-50 shadow">
-                        <h5 class="text-xs pb-2 text-gray-800/80 px-4">Search Result</h5>
-                        <p class="text-sm text-gray-500 py-1 hover:bg-gray-200/80 cursor-pointer px-4">Web Development</p>
-                        <p class="text-sm text-gray-500 py-1 hover:bg-gray-200/80 cursor-pointer px-4">IT service</p>
-                        <p class="text-sm text-gray-500 py-1 hover:bg-gray-200/80 cursor-pointer px-4">Iran</p>
-                        <p class="text-sm text-gray-500 py-1 hover:bg-gray-200/80 cursor-pointer px-4">Israel</p>
+                        class="absolute top-full left-0 right-0 mt-1.5 py-2 bg-white border border-gray-500/30 rounded-md overflow-hidden opacity-0 invisible transition-all duration-200 peer-focus:opacity-100 peer-focus:visible z-40 shadow text-start max-h-[200px] overflow-y-auto w-full">
+                        <h5 class="text-xs pb-2 text-gray-800/80 sm:px-4 px-2">Services</h5>
+                        <p class="text-sm text-gray-600 py-1 hover:bg-lime-200/80 cursor-pointer sm:px-4 px-2 text-nowrap">Web Development
+                        </p>
+                        <p class="text-sm text-gray-600 py-1 hover:bg-lime-200/80 cursor-pointer sm:px-4 px-2">IT service</p>
+                        <p class="text-sm text-gray-600 py-1 hover:bg-lime-200/80 cursor-pointer sm:px-4 px-2">Iran</p>
+                      
                     </div>
                 </div>
             </div>
             <span class="flex-1 h-full">
-                 <div class="flex flex-col gap-4 max-w-72 flex-1 h-full relative">
-                <div
-                    class="flex items-center px-3 gap-2 bg-white border-gray-500/30 h-full rounded-md overflow-hidden">
-                    <input type="text" placeholder="Where do u live"
-                        class="peer w-full h-full outline-none text-gray-500 placeholder-gray-500 text-sm">
-                        <i class="fa-solid fa-chevron-down  text-gray-400 "></i>
-
+                <div class="flex flex-col gap-4  w-full h-full relative">
                     <div
-                        class="absolute top-full left-0 right-0 mt-1.5 py-2 bg-white border border-gray-500/30 rounded-md overflow-hidden opacity-0 invisible transition-all duration-200 peer-focus:opacity-100 peer-focus:visible z-50 shadow">
-                        <h5 class="text-xs pb-2 text-gray-800/80 px-4">Search Result</h5>
-                        <p class="text-sm text-gray-500 py-1 hover:bg-gray-200/80 cursor-pointer px-4">India</p>
-                        <p class="text-sm text-gray-500 py-1 hover:bg-gray-200/80 cursor-pointer px-4">Indonesia</p>
-                        <p class="text-sm text-gray-500 py-1 hover:bg-gray-200/80 cursor-pointer px-4">Iran</p>
-                        <p class="text-sm text-gray-500 py-1 hover:bg-gray-200/80 cursor-pointer px-4">Israel</p>
+                        class="flex items-center md:px-3 px-1.5  bg-white border-gray-500/30 h-full rounded-md overflow-hidden ">
+                        <input type="text" placeholder="Where do u live"
+                            class="peer w-full h-full outline-none text-gray-700 placeholder-gray-500 md:text-sm text-xs">
+                        <i class="fa-solid fa-chevron-down  text-gray-400 text-[10px] sm:text-base"></i>
+
+                        <div
+                            class="absolute top-full left-0 right-0 mt-1.5 py-2 bg-white border border-gray-500/30 rounded-md overflow-hidden opacity-0 invisible transition-all duration-200 peer-focus:opacity-100 peer-focus:visible z-40 shadow text-start max-h-[200px] overflow-y-auto w-full">
+                            <h5 class="text-xs pb-2 text-gray-800/80 sm:px-4 px-2">Search Result</h5>
+                            <p class="text-sm text-gray-600 py-1 hover:bg-lime-200/80 cursor-pointer sm:px-4 px-2">India</p>
+                            <p class="text-sm text-gray-600 py-1 hover:bg-lime-200/80 cursor-pointer sm:px-4 px-2">Indonesia</p>
+                            <p class="text-sm text-gray-600 py-1 hover:bg-lime-200/80 cursor-pointer sm:px-4 px-2">Iran</p>
+                        </div>
                     </div>
                 </div>
-            </div>
             </span>
 
-            <span
-                class="bg-lime-700 px-3 hover:bg-lime-600 rounded-md  text-lg h-full scale-x-[-1] flex items-center cursor-pointer">
+            <button
+                class="bg-lime-700 md:px-3 px-2 hover:bg-lime-600 rounded-md text-sm md:text-lg h-full scale-x-[-1] flex items-center cursor-pointer active:shadow-[inset_0_0_20px_rgba(0,0,0,0.3)]">
                 <i class="fa-solid fa-magnifying-glass"></i>
-            </span>
+            </button>
         </div>
-        <div class="flex gap-2 flex-wrap">
+        <div class="flex gap-2 md:justify-start justify-center flex-wrap">
             <span
-                class="bg-lime-950 text-lime-500 flex justify-center hover:bg-lime-900 items-center gap-2 text-xs px-2 py-2 font-medium rounded-md cursor-pointer">
-                Graphic Designs
+                class="bg-lime-950 text-gray-300 flex justify-center hover:bg-lime-900 items-center gap-1 text-sm px-2.5 py-1.5 font-medium rounded-md cursor-pointer">
+                <i class="fa-solid fa-palette  "></i>
+                <small>Graphic Designs</small>
+            </span>
+            <span
+                class="bg-lime-950 text-gray-300 flex justify-center hover:bg-lime-900 items-center gap-2 text-sm px-2.5 py-1.5 font-medium rounded-md cursor-pointer">
 
-                <svg width="12" height="12" viewBox="0 0 48 48" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M14 34L34 14M34 14H14M34 14V34" class="stroke-lime-500" stroke-width="4"
-                        stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
+                <i class="fa-solid fa-code  "></i>
+                <small>Web Development</small>
 
             </span>
             <span
-                class="bg-lime-950 text-lime-500 flex justify-center hover:bg-lime-900 items-center gap-2 text-xs px-2 py-2 font-medium rounded-md cursor-pointer">
-                Web Development
+                class="bg-lime-950 text-gray-300 flex justify-center hover:bg-lime-900 items-center gap-2 text-sm px-2.5 py-1.5 font-medium rounded-md cursor-pointer">
 
-                <svg width="12" height="12" viewBox="0 0 48 48" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M14 34L34 14M34 14H14M34 14V34" class="stroke-lime-500" stroke-width="4"
-                        stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
+
+                <i class="fa-solid fa-chart-line  "></i>
+                <small>SEO</small>
 
             </span>
             <span
-                class="bg-lime-950 text-lime-500 flex justify-center hover:bg-lime-900 items-center gap-2 text-xs px-2 py-2 font-medium rounded-md cursor-pointer">
-                SEO
+                class="bg-lime-950 text-gray-300 flex justify-center hover:bg-lime-900 items-center gap-2 text-sm px-2.5 py-1.5 font-medium rounded-md cursor-pointer">
 
-                <svg width="12" height="12" viewBox="0 0 48 48" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M14 34L34 14M34 14H14M34 14V34" class="stroke-lime-500" stroke-width="4"
-                        stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
 
+                <i class="fa-solid fa-film  "></i>
+                <small>Video Editing</small>
             </span>
             <span
-                class="bg-lime-950 text-lime-500 flex justify-center hover:bg-lime-900 items-center gap-2 text-xs px-2 py-2 font-medium rounded-md cursor-pointer">
-                Video Editing
+                class="bg-lime-950 text-gray-300 flex justify-center hover:bg-lime-900 items-center gap-2 text-sm px-2.5 py-1.5 font-medium rounded-md cursor-pointer">
 
-                <svg width="12" height="12" viewBox="0 0 48 48" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M14 34L34 14M34 14H14M34 14V34" class="stroke-lime-500" stroke-width="4"
-                        stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
 
-            </span>
-            <span
-                class="bg-lime-950 text-lime-500 flex justify-center hover:bg-lime-900 items-center gap-2 text-xs px-2 py-2 font-medium rounded-md cursor-pointer">
-                Website Designing
-
-                <svg width="12" height="12" viewBox="0 0 48 48" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M14 34L34 14M34 14H14M34 14V34" class="stroke-lime-500" stroke-width="4"
-                        stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
+                <i class="fa-solid fa-paintbrush  "></i>
+                <small>Website Designing</small>
 
             </span>
         </div>
     </div>
-    <div class="flex-1">
-        <img src="{{ asset('images/illustration.png') }}" alt="Team Meeting illustration" class="w-full h-full">
+    <div class="flex-2/5 min-w-[200px] max-w-[500px] ">
+        <img src="{{ asset('images/illustration.png') }}" alt="Team Meeting illustration" class="w-full h-full drop-shadow-[0_50px_20px_rgba(0,0,0,1)]">
     </div>
 </div>
