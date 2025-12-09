dd78c027805cf3c5a1249bf596ca45f121c1bdb4
Hamza <hamza47@gmail.com>
2025-12-09 21:27:06 +0500

    Ready Section

--- resources/views/home/insights.blade.php
+++ resources/views/home/insights.blade.php
@@ -1,100 +1,113 @@
-<div class="section w-full max-w-[1920px] flex flex-col gap-4 p-[50px] max-[720px]:p-5">
-        <h1 class="text-4xl text-black font-black">Insights & <span class="text-lime-700">Advice</span></h1>
-        <div class="flex w-full flex-wrap gap-2">
-            <div class="flex flex-col justify-between grow gap-1 w-[300px] hover:bg-lime-700/50 p-2 rounded-md">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiCNVtAT_eenIcH3kok4iNFaywR8dCmmIqqg&s"
-                    class="w-full h-[250px] object-cover rounded-md" alt="">
-                <h2 class="text-xl text-black font-bold">How AI Reshaping Development Workflows in 2025</h2>
-                <p class="text-sm font-semibold text-black">In the era of AI-driven development, Techreviewer conducts
-                    online
-                    surveys to track real-world trends in software engineering. We leverage our global network of top
-                    software development companies to assess the impact of AI on the development workflow.</p>
-                <div class="flex justify-between items-center">
-                    <span
-                        class="bg-lime-700 text-white flex w-fit justify-center hover:bg-lime-900 items-center gap-2 text-xs px-2 py-2 font-medium rounded-md cursor-pointer">
-                        Research
+<div class="section insights">
+    <h2 class="text-3xl sm:text-4xl md:text-5xl font-bold text-center">
+        Insights & <span class="text-(--primary)">Advice</span>
+    </h2>
 
+    <div class="swiper insights-swiper">
+        <div class="swiper-wrapper">
 
-                    </span>
-                    <span
-                        class="bg-lime-700 text-white flex w-fit justify-center hover:bg-lime-900 items-center gap-2 text-xs px-2 py-2 font-medium rounded-md cursor-pointer">
-                        17/11/2025 by TRC
-                    </span>
+            {{-- Insight 1 --}}
+            <div class="swiper-slide">
+                <div class="flex flex-col rounded-md gap-2 max-w-[600px] mx-auto">
+                    <h3 class="text-xl font-bold">How AI Reshaping Development Workflows in 2025</h3>
+                    <p class="text-sm font-semibold text-gray-600">
+                       In the era of AI-driven development, Techreviewer conducts online surveys to track real-world trends in software engineering. We leverage our global network of top software development companies to assess the impact of AI on the development workflow.
+                    </p>
+
+                    <div class="flex items-center gap-2">
+                        <div class="w-12 h-12 rounded-full overflow-hidden shrink-0">
+                            <img src="https://t3.ftcdn.net/jpg/03/59/42/28/360_F_359422840_6ZgJdJS5sOgXeDxYoFMrng55CzyVmK9j.jpg"
+                                alt="author-image" class="w-full h-full object-cover">
+                        </div>
+
+                        <address class="flex flex-col">
+                            <cite class="font-bold">Dods Bonkie:</cite>
+                            <small class="text-gray-500 font-semibold">
+                                Author of Ish company -
+                                <time datetime="2025-12-09">May 15, 2025</time>
+                            </small>
+                        </address>
+                    </div>
                 </div>
             </div>
-            <div class="flex flex-col justify-between grow gap-1 w-[300px] hover:bg-lime-700/50 p-2 rounded-md">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJklFQ7Zh3wQN0KEZbTz_g1iNFUD3PlqmyuA&s"
-                    class="w-full h-[250px] object-cover rounded-md" alt="">
-                <h2 class="text-xl text-black font-bold">Key Challenges in Medical Software Development: Navigating
-                    Regulatory
-                    Compliance and Data Privacy</h2>
-                <p class="text-sm font-semibold text-black">Software has quietly become the foundation of modern
-                    healthcare.
-                    From
-                    electronic medical records (EMR) systems and telemedicine apps to artificial intelligence tools that
-                    now aid in diagnostics, digital technologies are changing the way healthcare is delivered and
-                    managed.</p>
-                <div class="flex justify-between items-center">
-                    <span
-                        class="bg-lime-700 text-white flex w-fit justify-center hover:bg-lime-900 items-center gap-2 text-xs px-2 py-2 font-medium rounded-md cursor-pointer">
-                        Research
 
+            {{-- Insight 2 --}}
+            <div class="swiper-slide">
+                <div class="flex flex-col rounded-md gap-2 max-w-[600px] mx-auto">
+                    <h3 class="text-xl font-bold">Key Challenges in Medical Software Development</h3>
+                    <p class="text-sm font-semibold text-gray-600">
+                        Software has quietly become the foundation of modern healthcare. From electronic medical records (EMR) systems and telemedicine apps to artificial intelligence tools that now aid in diagnostics, digital technologies are changing the way healthcare is delivered and managed.
+                    </p>
+                    <div class="flex items-center gap-2">
+                        <div class="w-12 h-12 rounded-full overflow-hidden shrink-0">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-oS9pEVY7wK8PsU2nchUCgHYNxYKMYlCOYjwkn9voa7EgYag8BIIaIK3lXsDycUfZEpM&usqp=CAU"
+                                alt="author-image" class="w-full h-full object-cover">
+                        </div>
 
-                    </span>
-                    <span
-                        class="bg-lime-700 text-white flex w-fit justify-center hover:bg-lime-900 items-center gap-2 text-xs px-2 py-2 font-medium rounded-md cursor-pointer">
-                        17/11/2025 by TRC
-                    </span>
+                        <address class="flex flex-col">
+                            <cite class="font-bold">Ava Hartwell:</cite>
+                            <small class="text-gray-500 font-semibold">
+                                Author of NexaLogic Solutions -
+                                <time datetime="2025-12-09">May 15, 2025</time>
+                            </small>
+                        </address>
+                    </div>
                 </div>
             </div>
-            <div class="flex flex-col justify-between grow gap-1 w-[300px] hover:bg-lime-700/50 p-2 rounded-md">
-                <img src="https://res.cloudinary.com/dthpnue1d/image/upload/v1736932887/How_Much_Does_AI_Software_Development_Cost_A_Budgeting_Blueprint_for_Businesses_Banner_9ae66f6c89.webp"
-                    class="w-full h-[250px] object-cover rounded-md" alt="">
-                <h2 class="text-xl text-black font-bold">AI in Software Development 2025: From Exploration to
-                    Accountability –
-                    Survey-Based Analysis</h2>
-                <p class="text-sm font-semibold text-black">For the second year in a row, Techreviewer conducts an online
-                    survey on
-                    the role of AI in software development. We leverage our global network of software development
-                    providers to capture how Artificial Intelligence adoption is evolving across teams, geographies, and
-                    company sizes. </p>
-                <div class="flex justify-between items-center">
-                    <span
-                        class="bg-lime-700 text-white flex w-fit justify-center hover:bg-lime-900 items-center gap-2 text-xs px-2 py-2 font-medium rounded-md cursor-pointer">
-                        Research
 
+            {{-- Insight 3 --}}
+            <div class="swiper-slide">
+                <div class="flex flex-col rounded-md gap-2 max-w-[600px] mx-auto">
+                    <h3 class="text-xl font-bold">AI in Software Development 2025</h3>
+                    <p class="text-sm font-semibold text-gray-600">
+                        For the second year in a row, Techreviewer conducts an online survey on the role of AI in software development. We leverage our global network of software development providers to capture how Artificial Intelligence adoption is evolving across teams, geographies, and company sizes.
+                    </p>
 
-                    </span>
-                    <span
-                        class="bg-lime-700 text-white flex w-fit justify-center hover:bg-lime-900 items-center gap-2 text-xs px-2 py-2 font-medium rounded-md cursor-pointer">
-                        17/11/2025 by TRC
-                    </span>
+                    <div class="flex items-center gap-2">
+                        <div class="w-12 h-12 rounded-full overflow-hidden shrink-0">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1rSPF_JXuStwHIk_BmV-ti24icEt9oR4mhkhrUnPU8ZNVBlGUEVqzqgXWFtzNNgvBqJs&usqp=CAU"
+                                alt="author-image" class="w-full h-full object-cover">
+                        </div>
+
+                        <address class="flex flex-col">
+                            <cite class="font-bold">Julian Cross:</cite>
+                            <small class="text-gray-500 font-semibold">
+                                Author of BrightForge Technologies -
+                                <time datetime="2025-12-09">May 15, 2025</time>
+                            </small>
+                        </address>
+                    </div>
                 </div>
             </div>
-            <div class="flex flex-col justify-between grow gap-1 w-[300px] hover:bg-lime-700/50 p-2 rounded-md">
-                <img src="https://img.freepik.com/free-photo/futuristic-ai-chip-circuit-board_23-2151977487.jpg?t=st=1763474986~exp=1763478586~hmac=4243c7c6de6755b36439260d4f10abb08794a89db6f46ea661771f8555bc8b74&w=1060"
-                    class="w-full h-[250px] object-cover rounded-md" alt="">
-                <h2 class="text-xl text-black font-bold">AI in Software Development 2025: From Exploration to
-                    Accountability –
-                    Survey-Based Analysis</h2>
-                <p class="text-sm font-semibold text-black">For the second year in a row, Techreviewer conducts an online
-                    survey on
-                    the role of AI in software development. We leverage our global network of software development
-                    providers to capture how Artificial Intelligence adoption is evolving across teams, geographies, and
-                    company sizes. </p>
-                <div class="flex justify-between items-center">
-                    <span
-                        class="bg-lime-700 text-white flex w-fit justify-center hover:bg-lime-900 items-center gap-2 text-xs px-2 py-2 font-medium rounded-md cursor-pointer">
-                        Research
 
+            {{-- Insight 4 --}}
+            <div class="swiper-slide">
+                <div class="flex flex-col rounded-md gap-2 max-w-[600px] mx-auto">
+                    <h3 class="text-xl font-bold">Exploration to Accountability</h3>
+                    <p class="text-sm font-semibold text-gray-600">
+                        For the second year in a row, Techreviewer conducts an online survey on the role of AI in software development. We leverage our global network of software development providers to capture how Artificial Intelligence adoption is evolving across teams, geographies, and company sizes.
+                    </p>
 
-                    </span>
-                    <span
-                        class="bg-lime-700 text-white flex w-fit justify-center hover:bg-lime-900 items-center gap-2 text-xs px-2 py-2 font-medium rounded-md cursor-pointer">
-                        17/11/2025 by TRC
-                    </span>
+                    <div class="flex items-center gap-2">
+                        <div class="w-12 h-12 rounded-full overflow-hidden shrink-0">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuyY0HBrWy1_lgEDYuvcnvDP0O29g7gC9xTw&s"
+                                alt="author-image" class="w-full h-full object-cover">
+                        </div>
+
+                        <address class="flex flex-col">
+                            <cite class="font-bold">Mira Ellington:</cite>
+                            <small class="text-gray-500 font-semibold">
+                                Author of CodeSphere Innovations -
+                                <time datetime="2025-12-09">May 15, 2025</time>
+                            </small>
+                        </address>
+                    </div>
                 </div>
             </div>
+
         </div>
 
+        <!-- Pagination -->
+        <div class="swiper-pagination "></div>
     </div>
+</div>
