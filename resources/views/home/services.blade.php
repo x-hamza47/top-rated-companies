6f58a9e30e97be8c43efb934c819233e211cfb93
Hamza <hamzaamir72007@gmail.com>
2025-12-10 20:35:15 +0500

    Dynamic Services

--- resources/views/home/services.blade.php
+++ resources/views/home/services.blade.php
@@ -14,181 +14,38 @@
     {{-- Info: Cards --}}
     <div
         class="card-wrapper  my-4 lg:gap-x-20 md:gap-x-15 gap-x-5 gap-y-10 [&>div]:hover:border-lime-700 mobile-cards grid grid-cols-2 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-3 justify-items-center items-center relative">
-        {{-- !Card 1 --}}
-        <div
-            class="card relative rounded-md border-2 border-gray-500/40 flex flex-col sm:px-5 sm:py-4 px-3 py-2 gap-2 shadow-2xl w-full h-full">
-            <button class="close-btn absolute top-2 right-2 text-xl font-bold hidden"><i
-                    class="fa-solid fa-xmark  text-gray-500"></i></button>
-            <div class="card-title flex items-center gap-2 ">
-                <span class="bg-(--primary) px-1 py-1.5 rounded-md flex items-center justify-center">
-                    <i class="fa-solid fa-code text-white text-3xl"></i>
-                </span>
-                <h3 class="text-2xl font-bold text-(--secondary) text-center">Development</h3>
-            </div>
-            <div class="card-links mt-4 flex flex-col text-gray-600 font-bold gap-1">
-                <ul
-                    class="[&>li]:list-disc [&>li]:list-inside  [&>li]:mb-2 [&>li]:hover:text-(--light-primary) [&>li>*]:hover:text-(--light-primary)">
-                    <li><a href="">Mobile App Development</a></li>
-                    <li><a href="">Software Development</a></li>
-                    <li><a href="">Web Development</a></li>
-                    <li><a href="">AR/VR</a></li>
-                    <li><a href="">Artificial Intelligence</a></li>
-                    <li><a href="">Blockchain</a></li>
-                </ul>
+        @foreach ($categories as $category)
+            <div
+                class="card relative rounded-md border-2 border-gray-500/40 flex flex-col sm:px-5 sm:py-4 px-3 py-2 gap-2 shadow-2xl w-full h-full">
+                <button class="close-btn absolute top-2 right-2 text-xl font-bold hidden"><i
+                        class="fa-solid fa-xmark  text-gray-500"></i></button>
+                <div class="card-title flex items-center gap-2 ">
+                    <span class="bg-(--primary) px-1 py-1.5 rounded-md flex items-center justify-center">
+                        <i class="fa-solid {{ $category->icon }} text-white text-3xl"></i>
+                    </span>
+                    <h3 class="text-2xl font-bold text-(--secondary) text-center">{{ $category->name }}</h3>
+                </div>
+                <div class="card-links mt-4 flex flex-col text-gray-600 font-bold gap-1">
+                    <ul
+                        class="[&>li]:list-disc [&>li]:list-inside  [&>li]:mb-2 [&>li]:hover:text-(--light-primary) [&>li>*]:hover:text-(--light-primary)">
+                        @foreach ($category->subCategories as $sub)
+                            <li><a href="{{ route('services.companies', $sub->slug) }}">{{ $sub->name }}</a></li>
+                        @endforeach
+                    </ul>
 
+                </div>
+                <a href=""
+                    class="bg-(--secondary) cursor-pointer text-white w-full text-center rounded-md py-2 hover:bg-(--light-primary) font-semibold ">
+                    View More
+                </a>
             </div>
-            <a href=""
-                class="bg-(--secondary) cursor-pointer text-white w-full text-center rounded-md py-2 hover:bg-(--light-primary) font-semibold ">
-                View More
-            </a>
-        </div>
-        {{-- !Card 2 --}}
-        <div
-            class="card relative rounded-md border-2 border-gray-500/40 flex flex-col sm:px-5 sm:py-4 px-3 py-2 gap-2 shadow-2xl w-full h-full">
-            <button class="close-btn absolute top-2 right-2 text-xl font-bold sm:hidden hidden"><i
-                    class="fa-solid fa-xmark text-gray-500"></i></button>
-            <div class="card-title flex items-center gap-2 ">
-                <span class="bg-(--primary) px-1 py-1.5 rounded-md flex items-center justify-center">
-                    <i class="fa-solid fa-palette text-white text-3xl"></i>
-                </span>
-                <h3 class="text-2xl font-bold text-(--secondary) ">Designing</h3>
-            </div>
-            <div class="card-links mt-4 flex flex-col text-gray-600 font-bold gap-1">
-                <ul
-                    class="[&>li]:list-disc [&>li]:list-inside  [&>li]:mb-2 [&>li]:hover:text-(--light-primary) [&>li>*]:hover:text-(--light-primary)">
-                    <li><a href="">Web Design</a></li>
-                    <li><a href="">User Experience Design</a></li>
-                    <li><a href="">Logo Design</a></li>
-                    <li><a href="">Graphic Design</a></li>
-                    <li><a href="">Design</a></li>
-                    <li><a href="">Digital Design</a></li>
-                </ul>
-
-            </div>
-            <a href=""
-                class="bg-(--secondary) cursor-pointer text-white w-full text-center rounded-md py-2 hover:bg-(--light-primary) font-semibold">
-                View More
-            </a>
-        </div>
-        {{-- !Card 3 --}}
-        <div
-            class="card relative rounded-md border-2 border-gray-500/40 flex flex-col  sm:px-5 sm:py-4 px-3 py-2  gap-2 shadow-2xl w-full h-full">
-            <button class="close-btn absolute top-2 right-2 text-xl font-bold hidden"><i
-                    class="fa-solid fa-xmark text-gray-500"></i></button>
-            <div class="card-title flex items-center gap-2 ">
-                <span class="bg-(--primary) px-1 py-1.5 rounded-md flex items-center justify-center">
-                    <i class="fa-solid fa-chart-line text-white text-3xl"></i>
-                </span>
-                <h3 class="text-2xl font-bold text-(--secondary) ">Marketing</h3>
-            </div>
-            <div class="card-links mt-4 flex flex-col text-gray-600 font-bold gap-1">
-                <ul
-                    class="[&>li]:list-disc [&>li]:list-inside  [&>li]:mb-2 [&>li]:hover:text-(--light-primary) [&>li>*]:hover:text-(--light-primary)">
-                    <li><a href="">Digital Marketing</a></li>
-                    <li><a href="">SEO</a></li>
-                    <li><a href="">Social Media Marketing</a></li>
-                    <li><a href="">Mobile Marketing</a></li>
-                    <li><a href="">Content Marketing</a></li>
-                    <li><a href="">Search Marketing</a></li>
-                </ul>
-
-            </div>
-            <a href=""
-                class="bg-(--secondary) cursor-pointer text-white w-full text-center rounded-md py-2 hover:bg-(--light-primary) font-semibold">
-                View More
-            </a>
-        </div>
-        {{-- !Card 4 --}}
-        <div
-            class="card relative rounded-md border-2 border-gray-500/40 flex flex-col  sm:px-5 sm:py-4 px-3 py-2 gap-2 shadow-2xl w-full h-full">
-            <button class="close-btn absolute top-2 right-2 text-xl font-bold hidden"><i
-                    class="fa-solid fa-xmark text-gray-500"></i></button>
-            <div class="card-title flex items-center gap-2 ">
-                <span class="bg-(--primary) px-1 py-1.5 rounded-md flex items-center justify-center">
-                    <i class="fa-solid fa-bullhorn text-white text-3xl"></i>
-                </span>
-                <h3 class="text-2xl font-bold text-(--secondary) ">Advertising</h3>
-            </div>
-            <div class="card-links mt-4 flex flex-col text-gray-600 font-bold gap-1">
-                <ul
-                    class="[&>li]:list-disc [&>li]:list-inside  [&>li]:mb-2 [&>li]:hover:text-(--light-primary) [&>li>*]:hover:text-(--light-primary)">
-                    <li><a href="">Advertising</a></li>
-                    <li><a href="">Branding</a></li>
-                    <li><a href="">Creative</a></li>
-                    <li><a href="">Video Production</a></li>
-                    <li><a href="">Public Relations</a></li>
-                    <li><a href="">Media Planning & Buying</a></li>
-                </ul>
-
-            </div>
-            <a href=""
-                class="bg-(--secondary) cursor-pointer text-white w-full text-center rounded-md py-2 hover:bg-(--light-primary) font-semibold">
-                View More
-            </a>
-        </div>
-        {{-- !Card 5 --}}
-        <div
-            class="card relative rounded-md border-2 border-gray-500/40 flex flex-col  sm:px-5 sm:py-4 px-3 py-2  gap-2 shadow-2xl w-full h-full">
-            <button class="close-btn absolute top-2 right-2 text-xl font-bold hidden"><i
-                    class="fa-solid fa-xmark text-gray-500"></i></button>
-            <div class="card-title flex items-center gap-2 ">
-                <span class="bg-(--primary) px-1 py-1.5 rounded-md flex items-center justify-center">
-                    <i class="fa-solid fa-briefcase text-white text-3xl"></i>
-                </span>
-                <h3 class="text-2xl font-bold text-(--secondary) text-center ">Business</h3>
-            </div>
-            <div class="card-links mt-4 flex flex-col text-gray-600 font-bold gap-1">
-                <ul
-                    class="[&>li]:list-disc [&>li]:list-inside  [&>li]:mb-2 [&>li]:hover:text-(--light-primary) [&>li>*]:hover:text-(--light-primary)">
-                    <li><a href="">Call Centers</a></li>
-                    <li><a href="">BPO</a></li>
-                    <li><a href="">Accounting</a></li>
-                    <li><a href="">Commercial Real Estate</a></li>
-                    <li><a href="">HR Services</a></li>
-                    <li><a href="">Consulting</a></li>
-                </ul>
-
-            </div>
-            <a href=""
-                class="bg-(--secondary) cursor-pointer text-white w-full text-center rounded-md py-2 hover:bg-(--light-primary) font-semibold">
-                View More
-            </a>
-        </div>
-        {{-- !Card 6 --}}
-        <div
-            class="card relative rounded-md border-2 border-gray-500/40 flex flex-col  sm:px-5 sm:py-4 px-3 py-2 h-full gap-2 shadow-2xl w-full">
-            <button class="close-btn absolute top-2 right-2 text-xl font-bold hidden"><i
-                    class="fa-solid fa-xmark text-gray-500"></i></button>
-            <div class="card-title flex items-center gap-2">
-                <span class="bg-(--primary) px-1 py-1.5 rounded-md flex items-center justify-center">
-                    <i class="fa-solid fa-server text-white text-3xl"></i>
-                </span>
-                <h3 class="text-2xl font-bold text-(--secondary) ">IT Services</h3>
-            </div>
-            <div class="card-links mt-4 flex flex-col text-gray-600 font-bold gap-1">
-                <ul
-                    class="[&>li]:list-disc [&>li]:list-inside  [&>li]:mb-2 [&>li]:hover:text-(--light-primary) [&>li>*]:hover:text-(--light-primary)">
-                    <li><a href="">IT Services</a></li>
-                    <li><a href="">Cybersecurity</a></li>
-                    <li><a href="">Data Analytics</a></li>
-                    <li><a href="">Managed IT Services</a></li>
-                    <li><a href="">Cloud Consulting</a></li>
-                    <li><a href="">Staff Augmentation</a></li>
-                </ul>
-
-            </div>
-            <a href=""
-                class="bg-(--secondary) cursor-pointer text-white w-full text-center rounded-md py-2 hover:bg-(--light-primary) font-semibold">
-                View More
-            </a>
-        </div>
+        @endforeach
     </div>
 </div>
 
 @push('script')
     <script>
         const cards = document.querySelectorAll(".mobile-cards .card");
-
 
         cards.forEach(card => {
             const closeBtn = card.querySelector(".close-btn");
@@ -199,7 +56,7 @@
                     cards.forEach(c => c.classList.remove("active"));
                     card.classList.toggle("active");
 
-                    // e.stopPropagation();
+                    e.stopPropagation();
                 }
 
                 closeBtn.addEventListener("click", (e) => {
@@ -209,7 +66,6 @@
 
             })
         });
-
         document.addEventListener("click", (e) => {
             const activeCard = document.querySelector(".mobile-cards .card.active");
             if (!activeCard) return;
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
-use Illuminate\Support\Facades\Route;
 
-Route::view('/','home/home')->name('home');
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ServiceController;
+
+Route::get('/',[HomeController::class, 'index'])->name('home.index');
+Route::get('/companies/{subCategorySlug}', [ServiceController::class, 'index'])
+    ->name('services.companies');
 Route::view('/listicle', 'listicle');;
 
